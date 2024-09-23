0acd50509522a79e1e9362fe4e929f90ee8ec65f
Pikachuuu10 <erikajs616@gmail.com>
2024-09-23 01:38:16 -0700

    comit
    menambahkan tugas 2

---
+++ TUGAS 2/BankCustomer.php
@@ -0,0 +1,29 @@
+<?php
+include "orang.php";
+include "BankCustomer.php";
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello Praktikum2</h1>
+    <div class="div">
+        <?php
+            $nasabah = new BankCustomer();
+            $nasabah->setCustomerName('erika');
+            $nasabah->setAddress('indonesia');
+            $nasabah->setEmail('erikajs616@gmail.com');
+            $nasabah->setCard('Prioritas');
+            $nasabah->getAcc('adit');
+
+            $nasabah->insertCard();
+            $nasabah->enterPIN(1232);
+    ?>
+    </div>
+</body>
+</html>
+
+<!-- localhost/index.php?nama=erika -->
