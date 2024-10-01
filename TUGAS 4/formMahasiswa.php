2b4bbb57f9e797a15a63f251b1c938b158e03980
Pikachuuu10 <erikajs616@gmail.com>
2024-10-01 00:01:21 -0700

    Update formMahasiswa.php

--- TUGAS 4/formMahasiswa.php
+++ TUGAS 4/formMahasiswa.php
@@ -11,7 +11,6 @@ class form {
     public function setTextField($nama, $text)
     {
         $label = "<div class='wrapper'><label for='" . $nama . "'>" . $nama . "</label>";
-        // Perbaikan pada penulisan elemen input dan penutupan div yang benar
         $textfield = "<input class='form-input' type='text' name='" . $nama . "' value='" . $text . "'></div>";
         array_push($this->fields, $label . $textfield);
     }
@@ -22,7 +21,7 @@ class form {
         foreach ($this->fields as $field) {
             echo $field;
         }
-        // Tambahkan tombol submit
+       
         echo "<input type='submit' value='Simpan'>";
         echo "</form>";
     }
